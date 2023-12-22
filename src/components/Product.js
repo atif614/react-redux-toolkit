bc79749945c7800e28ba91cb6ffbc67aac7981ec
Atif Raza <razaatif20786@gmail.com>
2023-12-23 01:05:22 +0530

    created UI

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const Product = ()=>{
-    return(
+const Product = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        // console.log("useEffect");
+        fetch('https://fakestoreapi.com/products')
+            .then((products) => products.json())
+            .then((result) => { setProducts(result) });
+    }, [])
+    const cards = products.map((product) => {
+        return <div className="col-md-3" style={{ marginBottom: '10px' }}>
+            <Card key={product.id} className="h-100">
+                <div className="text-center">
+                    <Card.Img variant="top" src={product.image} style={{ width: '100px', height: '130px' }} />
+                </div>
+                <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>
+                        INR:{product.price}
+                    </Card.Text>
+                </Card.Body>
+                    <Card.Footer style={{ background: 'white' }}>
+                        <Button variant="primary">Add to Cart</Button>
+                    </Card.Footer>
+            </Card>
+        </div>
+    })
+    return (
         <>
-         <h1>Product Dashboard</h1>
+            <h1>Product Dashboard</h1>
+            <div className="row">
+                {cards}
+            </div>
         </>
     )
 }
